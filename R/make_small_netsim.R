8eef5f0f77faf58644854205f98a761be64430c8
Emily D Pollock <emilypo@union.csde.washington.edu>
2020-08-25 16:39:53 -0700

    let's try this commit again. deleting huge rdas and make new function to reduce size of netsims

---
+++ R/make_small_netsim.R
@@ -0,0 +1,29 @@
+#' @title Netsim Size Reduction
+#'
+#' @description Take objects and remove the cel.complete and misc variables I don't currently need
+#'
+#' @param x is the netsim output
+#' @export
+#'
+make_small_netsim <- function(x){
+  out <- list()
+  out$param <- x$param
+  out$control <- x$control
+  out$nwparam <- x$nwparam
+  keep <- c("num", "num.male", "num.feml", "meanAge", "propMale", "b.flow", "ds.flow", "marcoh.departed", "other.departed", "debutprop",
+            "coef.form.marcoh", "coef.form.casual", "meanDegMarcoh", "meanDegOther", "marcoh", "other", "other.new", "other.end",
+            "marcoh.new", "marcoh.end")
+  out$epi <- x$epi[keep]
+  out$el <- x$el
+
+  for (i in 1:x$control$nsims){
+    x$attr[[i]] <- x$attr[[i]][1:11]
+  }
+
+  out$attr <- x$attr
+  out$cel.temp <- x$cel.temp
+  class(out) <- "netsim"
+
+  return(out)
+}
+
