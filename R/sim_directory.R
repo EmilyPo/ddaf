5720ce1e733fec5de11f5b403d223a5d3aa4bf2b
Emily D Pollock <emilypo@union.csde.washington.edu>
2020-08-26 11:03:44 -0700

    including small netsims now, hopefully this works better

--- R/sim_directory.R
+++ R/sim_directory.R
@@ -1,16 +1,34 @@
 
 # in "july2020" "partner corrections"
-#yescrossterms_yesoffset_no65_eligible_nocorrections <- sim_5_5200_offest
-#yescrossterms_yesoffset_yes65_eligible_nocorrections <- sim_5_5200_offest_65
+#yescrossterms_yesoffset_no65_eligible_nocorrections <- make_small_netsim(sim_5_5200_offest)
+#usethis::use_data(yescrossterms_yesoffset_no65_eligible_nocorrections)
+
+#readRDS("~/Dissertation/Duration_Simulations/sims/July2020/partner_corrections/sim_5_5200_offest_65.rds")
+#yescrossterms_yesoffset_yes65_eligible_nocorrections <- make_small_netsim(sim_5_5200_offest_65)
+#usethis::use_data(yescrossterms_yesoffset_yes65_eligible_nocorrections)
 
 # in "july2020" "partner corrections" "nocross"
-#nocrossterms_yesoffset_no65_eligible_nocorrections <- sim_5_5200_offest_nocross
-
-# in debut
-#yescrossterms_nooffset_no65_debut_nocorrections <- sim_5_5200_og
+#sim_5_5200_offest_nocross <- readRDS("~/Dissertation/Duration_Simulations/sims/July2020/partner_corrections/nocross/sim_5_5200_offest_nocross.rds")
+#nocrossterms_yesoffset_no65_eligible_nocorrections <- make_small_netsim(sim_5_5200_offest_nocross)
+#usethis::use_data(nocrossterms_yesoffset_no65_eligible_nocorrections)
 
 # in "july2020" "debut"
-#nocrossterms_nooffset_no65_debut_nocorrections <- sim_5_5200_nocross
-#nocrossterms_yesoffset_no65_debut_nocorrections <- sim_5_5200_nocross_offset
-#nocrossterms_yesoffset_no65_debut_edapprox <- sim_5_5200_nocross_offset_edapprox
-#nocrossterms_yesoffset_yes65_debut_nocorrections <- sim_5_5200_nocross_offset_65
+#sim_5_5200_og <- readRDS("~/Dissertation/Duration_Simulations/sims/July2020/debut/sim_5_5200_og.rds")
+#yescrossterms_nooffset_no65_debut_nocorrections <- make_small_netsim(sim_5_5200_og)
+#usethis::use_data(yescrossterms_nooffset_no65_debut_nocorrections)
+
+#sim_5_5200_nocross <- readRDS("~/Dissertation/Duration_Simulations/sims/July2020/debut/sim_5_5200_nocross.rds")
+#nocrossterms_nooffset_no65_debut_nocorrections <- make_small_netsim(sim_5_5200_nocross)
+#usethis::use_data(nocrossterms_nooffset_no65_debut_nocorrections)
+
+#sim_5_5200_nocross_offset <- readRDS("~/Dissertation/Duration_Simulations/sims/July2020/debut/sim_5_5200_nocross_offset.rds")
+#nocrossterms_yesoffset_no65_debut_nocorrections <- make_small_netsim(sim_5_5200_nocross_offset)
+#usethis::use_data(nocrossterms_yesoffset_no65_debut_nocorrections)
+
+#sim_5_5200_nocross_offset_edapprox <- readRDS("~/Dissertation/Duration_Simulations/sims/July2020/debut/sim_5_5200_nocross_offset_edapprox.rds")
+#nocrossterms_yesoffset_no65_debut_edapprox <- make_small_netsim(sim_5_5200_nocross_offset_edapprox)
+#usethis::use_data(nocrossterms_yesoffset_no65_debut_edapprox)
+
+#sim_5_5200_nocross_offset_65 <- readRDS("~/Dissertation/Duration_Simulations/sims/July2020/debut/sim_5_5200_nocross_offset_65.rds")
+#nocrossterms_yesoffset_yes65_debut_nocorrections <- make_small_netsim(sim_5_5200_nocross_offset_65)
+#usethis::use_data(nocrossterms_yesoffset_yes65_debut_nocorrections)
